f016c1ef934ddaf07a252ad46e94613ee9dba7bb
Greg Boydston <42726472+Greg9485@users.noreply.github.com>
2022-01-02 07:47:39 -0500

    Angular Side of the Project

---
+++ Angular/src/app/friend/friend.ts
@@ -0,0 +1,12 @@
+export interface Friend{
+    id: number,
+    firstName: string,
+    lastName: string, 
+    dob: Date,
+    hobby: string,
+    likes: string,
+    dislikes: string,
+    imageUrl: string, 
+    relationship: string,
+    friendCode: string
+}
