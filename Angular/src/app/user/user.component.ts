0a3e77b1244123f210f599d0718e94b34fc0de39
Greg Boydston <42726472+Greg9485@users.noreply.github.com>
2022-01-02 07:55:20 -0500

    Add files via upload

---
+++ Angular/src/app/user/user.component.ts
@@ -0,0 +1,126 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit, Optional } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './user';
+import { UserService } from './user.service';
+/*
+import { Friend } from './friend/friend';
+import { FriendService } from './friend/friend.service';
+import { Gift } from './gift/gift';
+import { GiftService } from './gift/gift.service';
+*/
+
+@Component({
+//  selector: 'app-root',
+  templateUrl: './user.component.html',
+//  styleUrls: ['./app.component.css']
+})
+export class UserComponent implements OnInit{
+  title = 'AfterCake';
+  public users: User[];
+  public editUser: User|undefined;
+  public deleteUser: User|undefined;
+
+  //Do we need another AppComponent? Can 1 constructor be used for multiple compnents? See constructor below
+/*NEED TO MAKE A FRIENDS.COMPONENT.TS & A GIFTS.COMPONENT.TS
+  public friends: Friend[];
+  public editFriend: Friend|undefined;
+  public deleteFriend: Friend|undefined; 
+*/
+
+  constructor(private userService: UserService){
+    this.users = []
+  }
+
+  ngOnInit(){
+      this.getUsers();
+  }
+
+  public getUsers(): void{
+    this.userService.getUsers().subscribe(
+      (response: User[]) =>{
+        this.users = response; 
+      },
+      (error: HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    );
+  }
+
+  public onAddUser(addForm: NgForm): void{
+    document.getElementById('add-user-form')?.click();
+    this.userService.addUser(addForm.value).subscribe(
+      (response: User)=>{
+        console.log(response);
+        this.getUsers();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse)=>{
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdateUser(user: User): void{
+    this.userService.updateUser(user).subscribe(
+      (response: User)=>{
+        console.log(user);
+        this.getUsers();
+      },
+      (error: HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteUser(userId: number): void{
+    this.userService.deleteUser(userId).subscribe(
+      (response: void)=>{
+        console.log(response);
+        this.getUsers();
+      },
+      (error: HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    );
+  }
+
+  public searchUsers(key: string): void{
+    const results: User[]=[];
+    for(const user of this.users){
+      if(user.firstName.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      ||user.lastName.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      ||user.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      ||user.userName.toLowerCase().indexOf(key.toLowerCase()) !== -1){
+        results.push(user);
+      }
+    }
+    this.users = results;
+    if(results.length === 0 || !key){
+      this.getUsers();
+    }
+  }
+
+  public onOpenModal(user: User|undefined, mode: string): void{
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addUserModal');
+    }else if(mode === 'edit'){
+      this.editUser = user;
+      button.setAttribute('data-target', '#editUserModal');
+    }else if(mode === 'delete'){
+      this.deleteUser = user;
+      button.setAttribute('data-target', '#deleteUserModal');
+    }
+    if(container != null){
+      container.appendChild(button);
+      button.click();
+    }
+  }
+
+}
